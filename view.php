f395414883e8ac7d7ce21bd7c4f4ca099c3018e6
kanemcnamara <36803653+kanemcnamara@users.noreply.github.com>
2018-03-28 11:52:11 +1100

    Remove *Dead* Link

--- view.php
+++ view.php
@@ -11,7 +11,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<link rel="icon" href="res/img/rjfavicon.ico">
+	<link rel="icon" href="res/img/icon.ico"> <!--Favicon File path here -->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="refresh" content="60" >
 	<link rel="stylesheet" type="text/css" href="res/css/bootstrap.min.css">
